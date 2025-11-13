d49d0a795566b6dde4e3330c03368fc902a10696
kiet-ta <trananhkiet21082005@gmail.com>
2025-11-13 22:01:51 +0700

    make logo and create tray icon

--- spotifree/App.xaml.cs
+++ spotifree/App.xaml.cs
@@ -3,73 +3,95 @@ using spotifree.IServices;
 using spotifree.Models;
 using spotifree.Services;
 using spotifree.ViewModels;
-using System; // Thêm
-using System.IO; // Thêm
-using System.Data;
+using System.Drawing;
+using System.IO;
 using System.Windows;
+using System.Windows.Controls;
 
-namespace spotifree
+namespace spotifree;
+
+public partial class App : Application
 {
-    public partial class App : Application
-    {
-        private IServiceProvider _serviceProvider;
+    private IServiceProvider _serviceProvider;
+    private System.Windows.Forms.NotifyIcon _trayIcon;
 
-        // Các h?ng s? c?a b?n (gi? nguyên)
-        private const string ClientId = "5289d37828a5414485019a41cccad2bd";
-        private const string RedirectUri = "http://127.0.0.1:5173/callback";
-        private const string Scopes = "streaming user-read-email user-read-private " +
-                                      "user-read-playback-state user-modify-playback-state " +
-                                      "playlist-read-private playlist-modify-private " +
-                                      "user-read-recently-played user-top-read";
-        public IServiceProvider ServiceProvider => _serviceProvider;
-        public App()
-        {
-            IServiceCollection services = new ServiceCollection();
-            
-            // 1. Ch? g?i ConfigureServices
-            ConfigureServices(services);
-            
-            // 2. Build ServiceProvider M?T L?N DUY NH?T ? cu?i
-            _serviceProvider = services.BuildServiceProvider();
-        }
+    private const string ClientId = "5289d37828a5414485019a41cccad2bd";
 
-        private void ConfigureServices(IServiceCollection services)
-        {
-            // --- ??ng ký t?t c? service c?a b?n ? ?ÂY ---
+    private const string RedirectUri = "http://127.0.0.1:5173/callback";
 
-            // Services
-            services.AddSingleton(new SpotifyAuth(ClientId, RedirectUri, Scopes));
-            services.AddSingleton<ISpotifyService, SpotifyApi>();
-            services.AddSingleton<ILocalMusicService, LocalMusicService>();
-            services.AddSingleton<ISettingsService, SettingsService>();
+    private const string Scopes = "streaming user-read-email user-read-private " +
+                   "user-read-playback-state user-modify-playback-state " +
+                   "playlist-read-private playlist-modify-private " +
+                   "user-read-recently-played user-top-read";
 
-            services.AddSingleton<LocalLibraryService<LocalMusicTrack>>(provider =>
-            {
-                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "library.json");
-                return new LocalLibraryService<LocalMusicTrack>(path);
-            });
+    public IServiceProvider ServiceProvider => _serviceProvider;
 
-            // L?i c?a b?n ? ?ây:
-            // services.AddSingleton<ILocalLibraryService, LocalLibraryService>();
-            // ^^^ XÓA DÒNG NÀY. LocalLibraryService là generic (LocalLibraryService<T>)
-            // b?n không th? ??ng ký nó mà không có ki?u d? li?u.
+    public App()
+    {
+        IServiceCollection services = new ServiceCollection();
 
-            // ViewModels
-            services.AddTransient<MainViewModel>();
+        ConfigureServices(services);
 
-            // Windows
-            // AddSingleton cho c?a s? chính (ch? có 1)
-            services.AddSingleton<Spotifree>(); 
-            // AddTransient cho các c?a s? ph? (có th? m? nhi?u)
-            services.AddTransient<MusicDetail>();
-        }
-
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            // Ph?n này c?a b?n ?ã ?úng!
-            var spotifree = _serviceProvider.GetRequiredService<Spotifree>();
-            spotifree.Show();
-            base.OnStartup(e);
-        }
+        _serviceProvider = services.BuildServiceProvider();
     }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+
+        // Services
+        services.AddSingleton(new SpotifyAuth(ClientId, RedirectUri, Scopes));
+        services.AddSingleton<ISpotifyService, SpotifyApi>();
+        services.AddSingleton<ILocalMusicService, LocalMusicService>();
+        services.AddSingleton<ISettingsService, SettingsService>();
+
+        services.AddSingleton<LocalLibraryService<LocalMusicTrack>>(provider =>
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "library.json");
+            return new LocalLibraryService<LocalMusicTrack>(path);
+        });
+
+
+        services.AddTransient<MainViewModel>();
+
+        // Windows
+
+        services.AddSingleton<Spotifree>();
+        services.AddTransient<MusicDetail>();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var spotifree = _serviceProvider.GetRequiredService<Spotifree>();
+        spotifree.Show();
+        base.OnStartup(e);
+        ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+        _trayIcon = new System.Windows.Forms.NotifyIcon();
+        var uri = new Uri("pack://application:,,,/app.ico");
+        var streamInfo = Application.GetResourceStream(uri);
+        _trayIcon.Icon = new Icon(streamInfo.Stream);
+        _trayIcon.Text = "Spotifree";
+        _trayIcon.Visible = true;
+
+
+        var contextMenu = new System.Windows.Forms.ContextMenuStrip();
+        contextMenu.Items.Add("Open", null, (s, ev) => Open_Click(s, null));
+        contextMenu.Items.Add("Exit", null, (s, ev) => Exit_Click(s, null));
+
+        _trayIcon.ContextMenuStrip = contextMenu;
+
+        _trayIcon.DoubleClick += (s, ev) => Open_Click(s, null);
+    }
+    private void Open_Click(object sender, RoutedEventArgs e)
+    {
+        Current.MainWindow?.Show();
+        Current.MainWindow.WindowState = WindowState.Normal;
+        Current.MainWindow.Activate();
+    }
+
+    private void Exit_Click(object sender, RoutedEventArgs e)
+    {
+        Current.Shutdown();
+    }
+
 }
